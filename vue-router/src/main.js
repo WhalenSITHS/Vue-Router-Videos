187de0b5eb0f59e79ee455fa051213dcb9f90d49
mr. whalen <michael.whalen@sitechhs.com>
2022-01-28 10:11:45 -0500

    sdf

--- vue-router/src/main.js
+++ vue-router/src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/style.css";
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: function (h) {
+    return h(App);
+  },
+}).$mount("#app");
--- vue-router/src/views/Home.vue
+++ vue-router/src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  methods: {
+    fetchData: async function () {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`
+        );
+        const data = await response.json();
+        this.pokemons = data.results;
+        console.log(this.pokemons);
+      } catch (error) {
+        console.log("oops");
+      }
+    },
+  },
+
+  created: function () {
+    this.fetchData();
+  },
+};
 </script>
